a2b8834d7d302a42bf3a055e9d09673705c3a1b0
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2018-09-28 17:48:59 +0300

    1 attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  //функция поиска повторов
+  const countDuplicates = function(arr, compareNumber){
+    let test = 0;
+    for(let i = 0, len = arr.length; i < len; i++){
+      if(arr[i] === compareNumber){test++}
+    }
+    return test;
+  }
+
+  //поиск простых делитетей
+  let deviders =[];
+  let devided = base;
+  while(devided != 1){
+    for(let i = 2; i <= devided; i++){
+      if(devided % i === 0){
+        deviders.push(i);
+        devided /= i;
+        i = 1;
+      }
+    }
+  }
+
+  //вычисление  
+  let answer = 0;
+  for(let i = 0, len = deviders.length; i < len;i++){
+    let preAnswer = 0
+    for(let devider = deviders[i]; devider <= number; devider *= deviders[i]){
+      preAnswer += Math.floor(number/devider);
+    }
+    preAnswer = Math.floor(preAnswer / countDuplicates(deviders,deviders[i]));
+    if(preAnswer < answer || answer === 0){
+      answer = preAnswer;
+    }
+  }
+  return answer
 }
